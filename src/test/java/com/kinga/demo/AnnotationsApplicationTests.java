c33cbbf19481dac353ddd1be63e7a88bf40b20f2
mrakotomanana <rakotomanan@gmail.com>
2026-03-12 09:47:10 +0300

    first config

---
+++ src/test/java/com/kinga/demo/AnnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinga.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnotationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
